7252c151c64b8c88d083264e7c505eb2a6aa9b7a
Lamarre Patrice <patricelmrr@gmail.com>
2024-12-12 04:43:15 -0500

    actua11

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,27 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const dbConnection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+
+const dbConnection = new Sequelize(process.env.MYSQL, 
+    {
+        dialect: 'mysql',
+        logging: false
+    }
+)
+/*const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "mysql",
-});
+});*/
 
 
 module.exports = {dbConnection}
+
+/*
+const dbConnection = new Sequelize('proyecto-final', 'root', '', 
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    }
+)
+*/
