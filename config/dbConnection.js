85ae1d7cf9abf23bf8e1f4cb19988c781c67d25f
Lamarre Patrice <patricelmrr@gmail.com>
2024-12-12 04:52:21 -0500

    actue03

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -8,7 +8,7 @@ require('dotenv').config();
         logging: false
     }
 )*/
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const dbConnection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "mysql",
 });
